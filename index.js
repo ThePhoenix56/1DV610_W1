8cb55dcbada57d1d5852e2e1f639c331993ce5fc
Niklas Celik <nc222jr@student.lnu.se>
2026-09-11 11:44:18 +0200

    add totalSales function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+
+
+function totalSales(totalAmount, paidAmount){
+     
+    if(paidAmount >= totalAmount){
+
+        return console.log( `Your change is: ${paidAmount - totalAmount}`)
+
+    } else {
+        return console.log(`You havent paid enough. You have to pay ${Math.abs(paidAmount - totalAmount)}`)
+    }
+
+}
+
+
+totalSales(51, 50)
